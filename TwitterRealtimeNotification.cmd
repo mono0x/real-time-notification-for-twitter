fdb445721402e970fbd83abaf81be73829818981
mono <mono@monoweb.info>
2011-04-08 23:35:04 +0900

    Initial commit

---
+++ TwitterRealtimeNotification.cmd
@@ -0,0 +1,1 @@
+"%HOME%\AppData\Local\Google\Chrome\Application\chrome.exe" "--pack-extension=%CD%\TwitterRealtimeNotification" "--pack-extension-key=%CD%\TwitterRealtimeNotification.pem" --no-message-box
