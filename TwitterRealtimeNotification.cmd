3a0874c518bf16509612e5c006e294a1ca686d2d
mono <mono@monoweb.info>
2011-09-30 12:44:22 +0900

    Improved the build script

--- TwitterRealtimeNotification.cmd
+++
@@ -1,1 +0,0 @@
-"%HOME%\AppData\Local\Google\Chrome\Application\chrome.exe" "--pack-extension=%CD%\TwitterRealtimeNotification" "--pack-extension-key=%CD%\TwitterRealtimeNotification.pem" --no-message-box
