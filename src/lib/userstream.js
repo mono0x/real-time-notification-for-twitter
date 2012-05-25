d3db38c80c987f4ce29c98ebbf3b0c728e0a61f9
mono <mono@monoweb.info>
2012-05-25 21:18:35 +0900

    Update headers

--- src/lib/userstream.js
+++ src/lib/userstream.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 mono
+ * Copyright (c) 2011-2012 mono
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
